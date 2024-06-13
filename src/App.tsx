b27ff08b9f33c11079a5f754dcee61f4182319d1
Nikita Ganzha <nk8bnj@gmail.com>
2024-06-13 14:29:38 +0300

    add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import List from "./pages/List";
